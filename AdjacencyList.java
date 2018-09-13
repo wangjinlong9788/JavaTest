e54fc4b632c4bbf3daa178fbed45980c2a3ca43a
Wilson <wangjinlong98889@gmail.com>
2018-09-13 17:13:33 -0500

    Update AdjacencyList.java

--- AdjacencyList.java
+++ AdjacencyList.java
@@ -1,8 +1,41 @@
-Adjacency List Java
+/**Adjacency List Java
 We use Java Collections to store the Array of Linked Lists.
 
 class Graph
 {
     private int numVertices;
     private LinkedList<integer> adjLists[];
+}**/
+
+import java.io.*;
+import java.util.*;
+
+class Graph
+{
+    private int numVertices;
+    private LinkedList<Integer> adjLists[];
+ 
+    Graph(int vertices)
+    {
+        numVertices = vertices;
+        adjLists = new LinkedList[vertices];
+        
+        for (int i = 0; i < vertices; i++)
+            adjLists[i] = new LinkedList();
+    }
+ 
+    void addEdge(int src, int dest)
+    {
+        adjLists[src].add(dest);
+    }
+ 
+    public static void main(String args[])
+    {
+        Graph g = new Graph(4);
+ 
+         g.addEdge(0, 1);
+         g.addEdge(0, 2);
+         g.addEdge(1, 2);
+         g.addEdge(2, 3);
+    }
 }
