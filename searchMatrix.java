c073e7d91cf06f7704e0386b70a887f58db97ee5
Wilson <wangjinlong98889@gmail.com>
2018-10-07 01:12:37 -0500

    Add files via upload

---
+++ searchMatrix.java
@@ -0,0 +1,78 @@
+class searchMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+	
+	int row_num = matrix.length;
+	int col_num = matrix[0].length;
+	
+	int begin = 0, end = row_num * col_num - 1;
+	
+	while(begin <= end){
+		int mid = (begin + end) / 2;
+		int mid_value = matrix[mid/col_num][mid%col_num];
+		
+		if( mid_value == target){
+			return true;
+		
+		}else if(mid_value < target){
+			//Should move a bit further, otherwise dead loop.
+			begin = mid+1;
+		}else{
+			end = mid-1;
+		}
+	}
+	
+	return false;
+}
+}
+
+public class MainClass {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+          return new int[0];
+        }
+    
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+    
+    public static int[][] stringToInt2dArray(String input) {
+        JsonArray jsonArray = JsonArray.readFrom(input);
+        if (jsonArray.size() == 0) {
+          return new int[0][0];
+        }
+    
+        int[][] arr = new int[jsonArray.size()][];
+        for (int i = 0; i < arr.length; i++) {
+          JsonArray cols = jsonArray.get(i).asArray();
+          arr[i] = stringToIntegerArray(cols.toString());
+        }
+        return arr;
+    }
+    
+    public static String booleanToString(boolean input) {
+        return input ? "True" : "False";
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int[][] matrix = stringToInt2dArray(line);
+            line = in.readLine();
+            int target = Integer.parseInt(line);
+            
+            boolean ret = new Solution().searchMatrix(matrix, target);
+            
+            String out = booleanToString(ret);
+            
+            System.out.print(out);
+        }
+    }
+}
