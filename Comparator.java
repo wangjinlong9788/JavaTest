81896703e9b5013c0b0a3aa7d3d0e4a3bab698cf
Wilson <wangjinlong98889@gmail.com>
2018-10-17 17:28:50 -0500

    Add files via upload

---
+++ Comparator.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Player {
+	String name;
+	int score;
+
+	Player(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+}
+//https://www.hackerrank.com/challenges/ctci-comparator-sorting/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting&h_r=next-challenge&h_v=zen
+class Checker implements Comparator<Player> {
+    @Override
+    public int compare(Player a, Player b) {
+        if(a.score != b.score) {
+            return b.score - a.score;
+        }
+        return a.name.compareTo(b.name);
+    }
+}
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+        
+        for(int i = 0; i < n; i++){
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+
+        Arrays.sort(player, checker);
+        for(int i = 0; i < player.length; i++){
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
+    }
+}
